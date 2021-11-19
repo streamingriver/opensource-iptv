2390e28a532ab50c0da2ebd1fbaa3e18791328a5
Mindaugas Žvirblis <mz@9g.lt>
2021-11-19 09:26:15 +0200

    backend control widget

---
+++ app/Filament/Widgets/UpdateBackend.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\Widget;
+
+class UpdateBackend extends Widget
+{
+    public static $view = 'filament.widgets.update-backend';
+
+    public function updateBackend() {
+    }
+}
---
+++ resources/views/filament/widgets/update-backend.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <x-filament::card>
+        {{-- Widget content --}}
+        <h1>Backend control</h1>
+        <hr/>
+        <br/>
+        <button class="cursor-pointer font-medium border rounded transition duration-200 shadow-sm focus:ring focus:ring-opacity-50 border-transparent bg-primary-600 text-white hover:bg-primary-700 focus:ring-primary-200  text-sm py-2 px-4" wire:click="updateBackend" >Update Backend</button>
+    </x-filament::card>
+</div>
